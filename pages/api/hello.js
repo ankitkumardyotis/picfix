b6d18d61e19b41268c0a9720909843f3d9cf2e51
faiz khan <kfaiz2215@gmail.com>
2024-03-20 17:17:04 +0530

    some issue resolved

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,12 @@
 // pages/api/getPlan.js
 
 // import prisma from '@/lib/prisma';
-import fun from "@/lib/resend";
-
+// import fun from "@/lib/resend";
+// 
 export default async function handler(req, res) {
 
 
-    fun()
+    // fun()
 
 
     return res.status(200).json({ message: 'ypu are welcome' });
