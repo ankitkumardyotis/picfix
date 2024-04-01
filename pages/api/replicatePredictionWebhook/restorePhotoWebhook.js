f41b6817d5444a7b3525d75f9128184653063a42
faiz khan <kfaiz2215@gmail.com>
2024-04-01 13:01:57 +0530

    webhook intigration for replicate API

--- pages/api/replicatePredictionWebhook/restorePhotoWebhook.js
+++ pages/api/replicatePredictionWebhook/restorePhotoWebhook.js
@@ -5,28 +5,31 @@ export default async function handler(req, res) {
 
 
     console.log("You are in webhook configuration")
+    const output = []
 
 
     const successfulPrediction = req.body;
 
     if (successfulPrediction.status === 'succeeded') {
         console.log("successfulPrediction=================", successfulPrediction)
-        const eventData = {
-            rId: successfulPrediction.id,
-            created_at: successfulPrediction.created_at,
-            error: successfulPrediction.error,
-            model: successfulPrediction.model,
-            output: successfulPrediction.output,
-            status: successfulPrediction.status
 
-        }
+        // const eventData = {
+        //     rId: successfulPrediction.id,
+        //     created_at: successfulPrediction.created_at,
+        //     error: successfulPrediction.error,
+        //     model: successfulPrediction.model,
+        //     output: successfulPrediction.output,
+        //     status: successfulPrediction.status
+
+        // }
+        output.push(successfulPrediction.output)
 
         const saveWebhookData = await prisma.WebhookEvent.create({
             data: {
                 replicateId: successfulPrediction.id,
                 created_at: successfulPrediction.created_at,
                 model: successfulPrediction.model,
-                output: successfulPrediction.output,
+                output: output,
                 status: successfulPrediction.status
             },
         })
